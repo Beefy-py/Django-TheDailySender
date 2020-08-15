d8d81bef31ccc7aa2f37be1cb9ac75bbb1562989
beefykenny <hoftkenny@gmail.com>
2020-08-15 16:50:01 -0300

    added static root

--- data_sender/settings.py
+++ data_sender/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = os.environ['SENDER_EMAIL']
